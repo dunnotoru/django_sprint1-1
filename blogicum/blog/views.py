69b28590cb4c560513b6a3de35fee2a929366ebc
dunno <116356493+dunnotoru@users.noreply.github.com>
2024-07-17 17:50:24 +0700

    fix nessesary checeks

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -54,8 +55,15 @@ def index(request):
 
 
 def post_detail(request, id):
+    if not any(post['id'] == id for post in posts):
+        return HttpResponseNotFound(f'Поста с id {id} не существует.')
+
+    for p in posts:
+        if p['id'] == id:
+            required_post = p
+
     context = {
-        'post': posts[id]
+        'post': required_post
     }
     return render(request, 'blog/detail.html', context)
 
